f7579c7f9e7752cbb2d07607b25170e35d5e8fff
Muthukumar J <45478465+GitsMuthukumarJ@users.noreply.github.com>
2022-09-27 17:50:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+include_once 'Zoom_Api.php';
+
+$zoom_meeting = new Zoom_Api();
+
+$data = array();
+$data['topic'] 		= 'Example Test Meeting';
+//$data['start_date'] = date("Y-m-d h:i:s", strtotime('tomorrow'));
+$data['start_date'] = date("Y-m-d h:i:s", strtotime('today'));
+$data['duration'] 	= 1;
+$data['type'] 		= 2;
+$data['password'] 	= rand();
+
+try {
+	$response = $zoom_meeting->createMeeting($data);
+	
+	//echo "<pre>";
+	//print_r($response);
+	//echo "<pre>";
+	
+	echo "Meeting ID: ". $response->id;
+	echo "<br>";
+	echo "Topic: "	. $response->topic;
+	echo "<br>";
+	echo "Join URL: ". $response->join_url ."<a href='". $response->join_url ."'>Open URL</a>";
+	echo "<br>";
+	echo "Meeting Password: ". $response->password;
+    
+	
+} catch (Exception $ex) {
+    echo $ex;
+}
+
+
+?>
